a27d41499cc3135c6a75cba43cd75d6c46ce71d6
Ferenc Bondar <fhery021@gmail.com>
2020-03-11 16:15:02 +0200

    exercising dependency injection

---
+++ src/test/java/guru/springframework/sfgdi/controllers/ConstructorInjectedControllerTest.java
@@ -0,0 +1,21 @@
+package guru.springframework.sfgdi.controllers;
+
+import guru.springframework.sfgdi.services.GreetingServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class ConstructorInjectedControllerTest {
+
+    ConstructorInjectedController controller;
+
+    @BeforeEach
+    public void setUp() {
+        controller = new ConstructorInjectedController(new GreetingServiceImpl());
+    }
+
+    @Test
+    public void getGreetingTest() {
+        System.out.println(controller.getGreeting());
+    }
+
+}
