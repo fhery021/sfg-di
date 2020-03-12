fd66437b985d9d7cb8c22bba0263b4e995a355f5
Ferenc Bondar <fhery021@gmail.com>
2020-03-12 10:04:06 +0200

    bugs fixed

--- src/main/java/guru/springframework/sfgdi/services/I18nEnglishGreetingService.java
+++ src/main/java/guru/springframework/sfgdi/services/I18nEnglishGreetingService.java
@@ -1,5 +1,6 @@
 package guru.springframework.sfgdi.services;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
@@ -7,7 +8,8 @@ import org.springframework.stereotype.Service;
  * Created by jt on 12/27/19.
  */
 @Profile("EN")
-@Service("i18nService")
+@Qualifier("i18nService")
+@Service
 public class I18nEnglishGreetingService implements GreetingService {
     @Override
     public String sayGreeting() {
